f91ed9abe07ba40add0de65ae77e50f0aeab6ba4
fahrulrozifr7 <114669577+fahrulrozifr7@users.noreply.github.com>
2025-05-22 20:46:22 +0700

    Commit changes including app.json

--- server.js
+++ server.js
@@ -46,5 +46,5 @@ app.get('/', (req, res) => {
 // Start server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, '0.0.0.0', () => {
-  console.log(`🚀 Server running on http://localhost:${PORT}`);
+  console.log(`🚀 Server running on localhost:${PORT}`);
 });
