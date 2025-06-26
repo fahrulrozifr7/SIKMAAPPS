e3d327b98cc18cf88345317cf298f73bd9acc94d
fahrulrozifr7 <114669577+fahrulrozifr7@users.noreply.github.com>
2025-06-26 12:15:51 +0700

    Update pgConfig.js

--- config/pgConfig.js
+++ config/pgConfig.js
@@ -1,10 +1,10 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  user: process.env.PG_USER || 'postgres',
-  host: process.env.PG_HOST || 'localhost',
-  database: process.env.PG_DATABASE || 'appsikma',
-  password: process.env.PG_PASSWORD || 'fahrul1234',
+  user: process.env.PG_USER || 'xxxx',
+  host: process.env.PG_HOST || 'xxxxx',
+  database: process.env.PG_DATABASE || 'xxxxx',
+  password: process.env.PG_PASSWORD || 'xxxxxx',
   port: process.env.PG_PORT || 5432,
 });
 
